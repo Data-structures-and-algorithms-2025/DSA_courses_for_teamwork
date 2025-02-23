23bf2165df90b8d7346e5d6d776e593eee2d65d4
Osztian <144934374+osztianpalmarozalia@users.noreply.github.com>
2025-02-23 20:34:24 +0200

    ads

--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+#include "arrays/arrays.h"
+#include "utils/utils.h"
+
 int main(void) {
-    printf("Hello, World!\n");
+    /**
+     * TODO: read n from console - number of elements
+     * TODO: declare an arrays (no dynamic memory allocation needed)
+     * TODO: fill the arrays with random values between [1, 10]
+     * TODO: print the arrays
+     * TODO: calculate and print the number of prime numbers
+     */
+    int n;
+    scanf("%i",&n);
+    int array[n];
+    fillWithRandom(&array,n,1,10);
+    printArray(&array,n);
+
     return 0;
 }
---
+++ week-02/operations-with-numbers/utils/utils.c
@@ -0,0 +1,15 @@
+//
+// Created by palma on 2/23/2025.
+//
+
+#include "utils.h"
+
+#include <stdio.h>
+
+void printArray(int *array, int n)
+{
+    for (int i=0;i<n;++i)
+    {
+        printf("%i",array[i]);
+    }
+}
