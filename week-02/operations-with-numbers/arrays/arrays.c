cda692c8c63f6f40696005c708deafdc9bdebec6
fazlul24 <fbecst@gmail.com>
2025-03-03 09:54:55 +0200

    arrays

--- week-02/operations-with-numbers/arrays/arrays.c
+++ week-02/operations-with-numbers/arrays/arrays.c
@@ -2,4 +2,14 @@
 // Created by palma on 2/23/2025.
 //
 
+#include <stdlib.h>
+
+void fillWithRandom(int *array, int n, int start, int end) {
+    for (int i=0; i < n; i++) {
+        array[i] = rand() % (end - start + 1);
+    }
+}
+
+
+
 #include "arrays.h"
