ff7e04855e6ca8107d0c1e02def506428799b43b
funnymrmagyar <llevi1462@gmail.com>
2025-03-03 09:57:23 +0200

    main

--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#include "algorithms/algorithms.h"
+#include "arrays/arrays.h"
+#include "utils/utils.h"
+
 int main(void) {
     /**
      * TODO: read n from console - number of elements
@@ -8,5 +12,13 @@ int main(void) {
      * TODO: print the arrays
      * TODO: calculate and print the number of prime numbers
      */
+
+    int n;
+    printf("kerlek adj meg egy szamot\n");
+    scanf("%d",&n);
+    int tomb[n];
+    fillWithRandom(tomb,n,1,10);
+    printArray(tomb,n);
+    isPrime(n);
     return 0;
 }
