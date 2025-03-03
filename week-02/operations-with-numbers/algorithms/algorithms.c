fe4dd62ab8779272ad99faaa22f7ab6fa06447be
BuksaAron <aronbuksa12@gmail.com>
2025-03-03 09:54:32 +0200

    classwork1

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,19 @@
 //
 
 #include "algorithms.h"
+
+#include <stdio.h>
+#include <tgmath.h>
+
+bool isPrime(int number) {
+    if (number < 2) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    for (int i = 3; i < sqrt(number); i += 2) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
