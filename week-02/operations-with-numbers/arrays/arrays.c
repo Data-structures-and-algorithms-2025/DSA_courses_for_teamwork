27bd27efe8ee3c732967d7d06569344b5650351c
Nády Szabolcs <nady.szabolcs@gmail.com>
2025-03-05 11:43:32 +0200

    One student

--- week-02/operations-with-numbers/arrays/arrays.c
+++ week-02/operations-with-numbers/arrays/arrays.c
@@ -3,3 +3,11 @@
 //
 
 #include "arrays.h"
+
+#include <stdlib.h>
+
+void fillWithRandom(int *array, int n, int start, int end) {
+    for (int i = 0; i < n; ++i) {
+        array[i]=rand()%(end-start)+1+start;
+    }
+}
