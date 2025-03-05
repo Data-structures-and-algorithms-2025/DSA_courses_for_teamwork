e2fb7273ffd8bf8a46298f56368c38d04268f0c6
KisBenjamin <kis.beniamin.tibor@student.ms.sapientia.ro>
2025-03-05 17:22:07 +0200

    beni main.c es algorithms.c

--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#include "arrays/arrays.h"
+#include "utils/utils.h"
+#include "algorithms/algorithms.h"
+
 int main(void) {
     /**
      * TODO: read n from console - number of elements
@@ -8,5 +12,23 @@ int main(void) {
      * TODO: print the arrays
      * TODO: calculate and print the number of prime numbers
      */
+    int n;
+    scanf("%d",&n);
+    int array[n];
+    fillWithRandom(array,n,1,10);
+    printArray(array,n);
+
+
+    int sum=0;
+    for (int i=0;i<n;i++)
+    {
+        if (isPrime(array[i]))
+        {
+            sum++;
+        }
+    }
+
+    printf("Ennyi prim szam van: %d",sum);
+
     return 0;
 }
