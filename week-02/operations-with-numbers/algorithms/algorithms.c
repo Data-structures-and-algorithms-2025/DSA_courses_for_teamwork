d68873e14c55c79d967dccdd9414dc315cf9e60d
Makkai-1 <makkaicsongor5@gmail.com>
2025-03-10 08:08:30 +0200

    Array + algorithms

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,13 @@
 //
 
 #include "algorithms.h"
+
+bool isPrime(int number) {
+    bool prim=true;
+
+    if (number < 2){ prim = true;}  // 0 és 1 nem prím
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) {prim = false;}  // Ha osztható, nem prím
+    }
+    return prim;
+}
