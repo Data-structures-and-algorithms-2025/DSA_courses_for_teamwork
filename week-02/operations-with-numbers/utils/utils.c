c296ccc1fc4e419e832fc473b4c77c5ef923c6ae
Fulori4 <mathe.lorant@student.ms.sapientia.ro>
2025-03-10 08:26:41 +0200

    printArray

--- week-02/operations-with-numbers/utils/utils.c
+++ week-02/operations-with-numbers/utils/utils.c
@@ -3,8 +3,8 @@
 //
 #include <stdio.h>
 #include "utils.h"
-void printArray(int *array,int n){
-    for (int i=0;i<n;++i){
-        printf("%d",array[i]);
+void printArray(int *array,int n) {
+    for (int i = 0; i < n; ++i) {
+        printf("%d", array[i]);
     }
 }
