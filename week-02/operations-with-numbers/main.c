da4b36d6fdc58b0240a44feba7d7dd7ff34f4a33
WoTrox <wotroxgp@gmail.com>
2025-03-10 08:49:46 +0200

    main, print dont

--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "algorithms/algorithms.h"
+#include "arrays/arrays.h"
+#include "utils/utils.h"
 
 int main(void) {
     /**
@@ -8,5 +13,21 @@ int main(void) {
      * TODO: print the arrays
      * TODO: calculate and print the number of prime numbers
      */
+
+    int n;
+    scanf("%d", &n);
+    int array[n] = {};
+
+    fillWithRandom(array, n, 1, 10);
+
+    printArray(array, n);
+
+    int primes = 0;
+    for (int i = 0; i < n; i++) {
+        if (isPrime(array[i])) {
+            primes++;
+        }
+    }
+    printf("Primes: %d\n", primes);
     return 0;
 }
