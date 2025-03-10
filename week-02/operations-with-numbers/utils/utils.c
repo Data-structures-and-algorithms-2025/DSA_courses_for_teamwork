42317568b240f51c1d2be86fcbe150c8a5acf390
DaniB555 <bartos.daniel@student.ms.sapientia.ro>
2025-03-10 08:30:05 +0200

    Update utils.c

--- week-02/operations-with-numbers/utils/utils.c
+++ week-02/operations-with-numbers/utils/utils.c
@@ -8,6 +8,10 @@
 
 void printArray(int *array, int n) {
     for(int i=0;i<n;i++) {
-        printf("%i",array[i]);
+        for(int j=0;j<n;j++) {
+            printf("%i ",array[i]);
+        }
+        printf("\n");
     }
+
 }
