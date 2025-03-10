ca2612b044018c178f5b67401a62c63cf251c0ab
Tlehel <tulit.lehel@student.ms.sapientia.ro>
2025-03-10 08:05:53 +0200

    isprime comit

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,18 @@
 //
 
 #include "algorithms.h"
+
+bool isPrime(int number){
+  if(number <= 1){
+    return false;
+  }
+  if(number == 2){
+    return true;
+  }
+  for (int i=3;i*i<=number;i+=2){
+    if(number % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
