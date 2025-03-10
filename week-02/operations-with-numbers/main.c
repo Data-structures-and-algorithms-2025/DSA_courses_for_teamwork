2e244e7fa9604f6cc72b2fd7c53844216a8e1553
RomanMate <romanmate61@gmail.com>
2025-03-10 11:36:54 +0200

    exercise

--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#include "algorithms/algorithms.h"
+#include "arrays/arrays.h"
+#include "utils/utils.h"
+
 int main(void) {
     /**
      * TODO: read n from console - number of elements
@@ -8,5 +12,15 @@ int main(void) {
      * TODO: print the arrays
      * TODO: calculate and print the number of prime numbers
      */
+    int n;
+    scanf("%i",&n);
+    int array[n];
+    fillWithRandom(array,n,1,10);
+    printArray(array,n);
+    for(int i=0;i<=n;i++) {
+        if(isPrime(array[i])==true) {
+            
+        }
+    }
     return 0;
 }
--- week-02/operations-with-numbers/utils/utils.c
+++ week-02/operations-with-numbers/utils/utils.c
@@ -1,5 +1,10 @@
 //
 // Created by palma on 2/23/2025.
 //
-
+#include <stdio.h>
 #include "utils.h"
+void printArray(int *array, int n) {
+    for(int i=0;i<n;i++) {
+        printf("%i",array[i]);
+    }
+}
