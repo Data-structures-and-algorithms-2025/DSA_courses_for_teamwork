7c631f9f6db69b4fdb96689176e74c6f4f079f01
emoke30 <szenneremoke@gmail.com>
2025-03-10 08:48:32 +0200

    isPrime

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,15 @@
 //
 
 #include "algorithms.h"
+
+bool isPrime(int number) {
+    if (number<=1) {
+        return false;
+    }
+    for (int i=2; i*i<number; i++) {
+        if (number%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
