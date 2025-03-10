e3452eb5210e323aa7d2dc1cf22a07943ed16bf1
AndorH1 <hegyeli.attila@student.ms.sapientia.ro>
2025-03-10 08:10:06 +0200

    Algorithms bool is prime

--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -5,7 +5,7 @@ int main(void) {
      * TODO: read n from console - number of elements
      * TODO: declare an arrays (no dynamic memory allocation needed)
      * TODO: fill the arrays with random values between [1, 10]
-     * TODO: print the arrays
+     * TODO: print the array
      * TODO: calculate and print the number of prime numbers
      */
     return 0;
