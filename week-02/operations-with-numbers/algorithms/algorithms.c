d71c5562ce5c6cf527f66477a61655f67ae6c3b9
KelemenAkos <keliakos05@gmail.com>
2025-03-10 08:22:30 +0200

    DONE :)

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,15 @@
 //
 
 #include "algorithms.h"
+
+#include <math.h>
+
+bool isPrime(int number) {
+    if (number < 2) {return false; }
+    for (int i=2; i<=sqrt(number); i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
--- week-02/operations-with-numbers/main.c
+++ week-02/operations-with-numbers/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "algorithms/algorithms.h"
+#include "arrays/arrays.h"
+#include "utils/utils.h"
 
 int main(void) {
     /**
@@ -8,5 +13,20 @@ int main(void) {
      * TODO: print the arrays
      * TODO: calculate and print the number of prime number
      */
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int array[n];
+    fillWithRandom(array, n, 1, 10);
+    printf("\nThe random generated array elements:\n ");
+    printArray(array, n);
+    printf("\nChecking how many prime elements are in sequence\n");
+    int num_prime = 0;
+    for (int i = 0; i < n; i++) {
+        if (isPrime(array[i])) {
+            num_prime++;
+        }
+    }
+    printf("\nNumber of prime elements: %d\n", num_prime);
     return 0;
 }
