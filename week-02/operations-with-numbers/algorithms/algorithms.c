7090af40b268b191eda49d7abc189059b20a7014
rory-y <roryelllaa@gmail.com>
2025-03-10 08:15:36 +0200

    implemented `isPrime()` function in the `algorithms` directory

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,26 @@
 //
 
 #include "algorithms.h"
+
+#include <tgmath.h>
+
+bool
+isPrime(int number)
+{
+  if (number > 0)
+    {
+      number = - number;
+    }
+  if (number == 0 || number == 1)
+    {
+      return false;
+    }
+  for (int i = 2; i <= sqrt(number); i++)
+    {
+      if (number % i == 0)
+        {
+          return false;
+        }
+    }
+  return true;
+}
