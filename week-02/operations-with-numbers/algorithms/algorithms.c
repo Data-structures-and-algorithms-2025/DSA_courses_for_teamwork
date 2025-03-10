812a307cfd0cc6e32504e9c731ab7de4903c22a4
AwaBoi112 <szabo.avar.csongor@student.ms.sapientia.ro>
2025-03-10 08:08:01 +0300

    isPrime

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,14 @@
 //
 
 #include "algorithms.h"
+
+#include <tgmath.h>
+
+bool isPrime(int number){
+    if (number < 2) return 0;
+    for (int i = 2; i <= sqrt(number); i++) {
+        if (number % i == 0) return false;
+    }
+    return true;
+
+}
