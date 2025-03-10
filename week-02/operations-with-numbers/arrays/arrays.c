426a377bd5a0fcfea909d5f62884594f93d494cc
Am3nt <andrasmihaly13@gmail.com>
2025-03-10 08:37:34 +0200

    Week 2 array

--- week-02/operations-with-numbers/arrays/arrays.c
+++ week-02/operations-with-numbers/arrays/arrays.c
@@ -9,7 +9,6 @@
 
 void fillWithRandom(int *array, int n, int start, int end) {
     srand(time(0));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         array[i] = rand() % (end - start + 1);
-    }
 }
