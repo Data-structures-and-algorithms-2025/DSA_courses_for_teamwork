7df5bd9e1b6a2425638118b4600224bd61560548
BalazsSapi <domokos.balazs@student.ms.sapientia.ro>
2025-03-10 08:44:36 +0200

    isPrime fixed in algorithms.c

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -10,6 +10,9 @@ bool isPrime(int number){
     if (number <= 1) {
         return false;
     }
+    if (number == 2) {
+        return true;
+    }
     if (number %2== 0) {
         return false;
     }
