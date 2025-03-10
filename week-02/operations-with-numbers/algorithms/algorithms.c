3504c5957bb8989d432869df6b02a4d1d8510a83
MezofiTihamer <mtihamer123@gmail.com>
2025-03-10 08:25:56 +0200

    Update algorithms.c

--- week-02/operations-with-numbers/algorithms/algorithms.c
+++ week-02/operations-with-numbers/algorithms/algorithms.c
@@ -3,3 +3,19 @@
 //
 
 #include "algorithms.h"
+
+#include <math.h>
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    if (number == 2) {
+        return  true;
+    }
+    for (int i = 2; i < sqrt(number); i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+}
