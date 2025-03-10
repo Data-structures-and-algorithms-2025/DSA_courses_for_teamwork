e3b64c1a50d56f42fe622580aa87500a411030db
Koppany26 <kollokoppany26@gmail.com>
2025-03-10 08:37:21 +0200

    fillWithRandom

--- week-02/operations-with-numbers/arrays/arrays.c
+++ week-02/operations-with-numbers/arrays/arrays.c
@@ -3,3 +3,16 @@
 //
 
 #include "arrays.h"
+#include "stdlib.h"
+#include "time.h"
+#include "stdio.h"
+
+
+void fillWithRandom(int *array, int n, int start, int end) {
+    srand(time(0));
+    for (int i=0;i<n;i++) {
+        array[i]=(end-start)*rand()/RAND_MAX*start;
+        //=start + rand() % (end - start + 1);
+    }
+}
+
