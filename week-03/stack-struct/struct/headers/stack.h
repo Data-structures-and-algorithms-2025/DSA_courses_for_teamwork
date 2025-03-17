fc6a78d73a28984afe36d30a2814e9003be70af8
zolcseakdora <zolcseak.dora@student.ms.sapientia.ro>
2025-03-17 09:06:56 +0200

    struct stack letrehozva

---
+++ week-03/stack-struct/struct/headers/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by Dora on 2025. 03. 17..
+//
+
+#ifndef STACK_H
+#define STACK_H
+
+#endif //STACK_H
---
+++ week-03/stack-struct/struct/sources/stack.c
@@ -0,0 +1,5 @@
+//
+// Created by Dora on 2025. 03. 17..
+//
+
+#include "stack.h"
