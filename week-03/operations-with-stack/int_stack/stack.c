f3e813b7523c51eacabe81ada4e22e1c300160d0
CeFgon <105783585+CeFgon@users.noreply.github.com>
2025-03-17 09:54:52 +0200

    Stack done
    Hunor's work

--- week-03/operations-with-stack/int_stack/stack.c
+++ week-03/operations-with-stack/int_stack/stack.c
@@ -3,3 +3,52 @@
 //
 
 #include "stack.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void createStack(int capacity, Stack_t *stack) {
+    stack->top=-1;
+    stack->elements=(int*)malloc(sizeof(int)*capacity);
+    if (!stack->elements) {
+        printf("Memory allocation error");
+    }
+}
+
+void destroyStack(Stack_t *stack) {
+    stack->top=-1;
+    free(stack->elements);
+}
+
+bool isFull(Stack_t stack) {
+    return stack.top == stack.capacity;
+}
+
+int isEmpty(Stack_t stack) {
+    return  stack.top == -1;
+}
+
+void push(Stack_t *stack, int item) {
+    if (!isFull(*stack))
+    stack->elements[++stack->top]=item;
+    else {
+        printf("\nStack is Full\n");
+    }
+}
+
+int pop(Stack_t *stack) {
+    if (!isEmpty(*stack))
+    stack->top-=1;
+    else {
+        printf("Stack is empty");
+        return -3;
+    }
+}
+
+int peek(Stack_t stack) {
+    return stack.elements[stack.top];
+}
+
+int size(Stack_t stack) {
+    return stack.top;
+}
--- week-03/operations-with-stack/main.c
+++ week-03/operations-with-stack/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void)
-{
+#include "int_stack/stack.h"
+
+int main(void) {
     /*Egy kezdetben üres, maximum 4 kapacitású verembe próbáljuk elhelyezni, ebben a sorrendben, az 1,2,3,4,5 számokat.
     a) Vegyünk ki két értéket, majd próbáljuk betenni, ebben a sorrendben a 6,7 és 8 számokat.
     b) Utána vegyünk ki 1 értéket.
@@ -9,6 +10,29 @@ int main(void)
     d) Mennyi lesz a veremben található elemek összege a leírt műveletek elvégzése után?*/
     //A vermet nem szabad bejárni!!!
     //A main-ben ne hivatkozz a struktúra mezőire!!!
-
+    Stack_t stack;
+    stack.capacity=4;
+    createStack(stack.capacity,&stack);
+    for (int i=0; i<=stack.capacity; ++i) {
+        push(&stack,i+1);
+    }
+    printf("\nTorolt ertek: %d\n",stack.elements[stack.top]);
+    pop(&stack);
+    printf("\nTorolt ertek: %d\n",stack.elements[stack.top]);
+    pop(&stack);
+    /// a) masodik fele
+    push(&stack,6);
+    push(&stack,7);
+    push(&stack,8);
+    /// b)
+    pop(&stack);
+    printf("\nc) A szam a verem tetejen: %d\n",peek(stack));
+    /// d) A veremben talalhato elemek osszege
+    int x=stack.top, sum=0;
+    for (int i=0;i<x;++i) {
+        sum+=peek(stack);
+        pop(&stack);
+    }
+    printf("\nA verem elemeinek osszege:%d\n",sum);
     return 0;
 }
