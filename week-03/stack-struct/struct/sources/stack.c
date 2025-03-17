2e5dfde01efd58647b912735853c5f1636863930
zolcseakdora <zolcseak.dora@student.ms.sapientia.ro>
2025-03-17 09:31:50 +0200

    a fgv definiciok

--- week-03/stack-struct/struct/sources/stack.c
+++ week-03/stack-struct/struct/sources/stack.c
@@ -3,3 +3,42 @@
 //
 
 #include "stack.h"
+#include <stdbool.h>
+
+bool isFull(Stack_t stack){
+    if(stack.top==stack.capacity){
+      return true;
+    }
+    return false;
+}
+bool isEmpty(Stack_t stack){
+   if (stack.top==0)
+    {
+        return true;
+    }
+    return false;
+}
+void push(Stack_t* stack, int item){
+  if(isFull(stack)){
+    printf("A verem tele van");
+    return -1;
+  }
+  return stack.elements[++stack.top]=item;
+}
+int pop(Stack_t* stack){
+  if(isEmpty(stack)){
+    printf("A verem ures");
+    return -1;
+  }
+  return stack.elements[stack.top--];
+}
+int peek(Stack_t stack){
+  if(isEmpty(stack)){
+    printf("A verem ures");
+    return -1;
+  }
+  return stack.elements[stack.top];
+}
+int size(Stack_t stack) {
+    return stack.top + 1;
+}
