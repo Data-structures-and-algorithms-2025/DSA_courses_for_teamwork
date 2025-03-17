6da33e0f1195fe59f26f01878cbf03bccef4dbf3
Kovacs1Dani <dk.kovacs123@gmail.com>
2025-03-17 09:38:53 +0200

    reading details added

--- week-03/operations-with-struct/product/product.c
+++ week-03/operations-with-struct/product/product.c
@@ -3,3 +3,14 @@
 //
 
 #include "product.h"
+#include <stdio.h>
+
+void readProductDetails(Product_t *pProduct) {
+    fscanf(stdin, " %30[^\n]", pProduct->name);
+    fscanf(stdin, " %10s", pProduct->barCode);
+    fscanf(stdin, " %50[^\n]", pProduct->supplier);
+    fscanf(stdin, "%d %d %d", &pProduct->date.year, &pProduct->date.month, &pProduct->date.day);
+    fscanf(stdin, "%d", (int*)&pProduct->category);
+    fscanf(stdin, "%f", &pProduct->price);
+
+}
