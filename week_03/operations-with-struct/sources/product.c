b98b05e77b20d51dcd9543e6181621a4696fedbd
Munlaly <nemetdbence@gmail.com>
2025-03-17 09:13:59 +0200

    initialization

---
+++ week_03/operations-with-struct/sources/product.c
@@ -0,0 +1,5 @@
+//
+// Created by Bence on 3/17/2025.
+//
+
+#include "product.h"
---
+++ week_03/operations-with-struct/sources/product.h
@@ -0,0 +1,8 @@
+//
+// Created by Bence on 3/17/2025.
+//
+
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#endif //PRODUCT_H
