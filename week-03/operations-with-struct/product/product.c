8c99e0d3a37d2c6ff861e9c4ea11f0d78c5e5c87
TothArminTamas <toth.armin.tamas@student.ms.sapientia.ro>
2025-03-17 09:46:42 +0200

    Quick Quokkas week03 operations with struct

--- week-03/operations-with-struct/product/product.c
+++ week-03/operations-with-struct/product/product.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+#include "product.h"
+
 //
 // Created by oszti on 3/16/2025.
 //
+void readProductDetails(Product_t *pProduct) {
+        scanf("%[^\n]\n", pProduct->name);
+        scanf("%s\n",pProduct->barcode);
+        scanf("%[^\n]\n", pProduct->supplier);
+        scanf("%d%d%d\n", &pProduct->dateOfManufacture.year, &pProduct->dateOfManufacture.month,  &pProduct->dateOfManufacture.day);
+        scanf("%i", pProduct->prodType);
+        scanf("%f", pProduct->price);
+}
+void printProduct(Product_t product) {
+        printf("The product's name: %s\n", product.name);
 
-#include "product.h"
+}
--- week-03/operations-with-struct/product/product.h
+++ week-03/operations-with-struct/product/product.h
@@ -2,8 +2,7 @@
 // Created by oszti on 3/16/2025.
 //
 
-#ifndef PRODUCT_H
-#define PRODUCT_H
+
 /*Definiálj egy Product_t címkéjű struktúrát, ha a következő információkat tudjuk egy termékről:
 •	name (termék neve, max 30 karakter)
 •	barcode (termék kódja, max 10 karakter)
@@ -14,16 +13,29 @@ o	egyszerűbb változat: csak yearOfManufacture
 •	category (termék típusa, Enum: FRUIT, VEGETABLE, DIARY, OTHER)
 •	price (a termék ára/ db vagy /kg)
 Adottak a következő product.h fájlban megadott függvény deklarációk. Implementáld a függvényeket a product.c fájlban.*/
-
+enum Type{FRUIT, VEGETABLE, DIARY,OTHER};
+typedef struct {
+    int year, month, day;
+}Date_t;
+typedef struct{
+    char name[30];
+    char barcode[10];
+    char supplier[50];
+    Date_t dateOfManufacture;
+    enum Type prodType;
+    float price;
+}Product_t;
+#ifndef PRODUCT_H
+#define PRODUCT_H
 /**
 Reads all details of a product, product must be passed by reference
 @param pProduct - pointer to a product
 */
-//void readProductDetails(Product_t *pProduct);
+void readProductDetails(Product_t *pProduct);
 /**
 Prints all details of a product
 @param product
 */
-//void printProduct(Product_t product);
+void printProduct(Product_t product);
 
 #endif //PRODUCT_H
