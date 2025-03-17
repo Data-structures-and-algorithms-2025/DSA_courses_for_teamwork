70b6ff4014bb01192afb9dc7b988baef585de980
Am3nt <andrasmihaly13@gmail.com>
2025-03-17 09:53:26 +0200

    Eload 3 verem

--- week-03/operations-with-stack/int_stack/stack.c
+++ week-03/operations-with-stack/int_stack/stack.c
@@ -3,3 +3,42 @@
 //
 
 #include "stack.h"
+#include <stdlib.h>
+
+void createStack(int capacity, Stack_t * stack){
+ stack=(Stack_t*)malloc(sizeof(Stack_t));
+}
+
+void destroyStack(Stack_t* stack) {
+ free(stack);
+}
+
+bool isFull(Stack_t stack) {
+ return(stack.top==stack.capacity);
+}
+
+bool isEmpty(Stack_t stack) {
+ if (stack.top==0)
+ return true;
+ return false;
+}
+
+void push(Stack_t* stack, int item) {
+ if ((*stack).top!=0) {
+  (*stack).top+=1;
+  (*stack).top*=item;
+ }
+}
+
+int pop(Stack_t* stack) {
+ (*stack).items[(*stack).top]=0;
+ (*stack).top-=1;
+}
+
+int peek(Stack_t stack) {
+ return stack.items[stack.top];
+}
+
+int size(Stack_t stack) {
+ return stack.top;
+}
