3d3461c20cd6519bea465819d3b6b3b72b7a0057
Mados Tamás <mados.tamas@student.ms.sapientia.ro>
2025-03-17 09:29:14 +0200

    Update stack.h

--- week-03/operations-with-stack/int_stack/stack.h
+++ week-03/operations-with-stack/int_stack/stack.h
@@ -10,56 +10,61 @@ Definiálj egy Stack címkéjű struktúrát, amely egy egész számokat tartalm
 •	top (a verem teteje – a legfelső elem pozíciója)
 •	items/elemek (egész számokat tartalmazó tömb)
 Adottak a következő stack.h fájlban megadott függvény deklarációk. Implementáld a függvényeket az stack.c fájlban.*/
+typedef struct Stack {
+    int capacity;
+    int top;
+    int *items;
+}Stack_t;
 /**
  * Initializing the capacity and size of stack,
  * allocating memory for the @elements array
  * @param capacity
  * @param stack
  */
-//void createStack(int capacity, Stack_t * stack);
+void createStack(int capacity, Stack_t * stack);
 /**
  * Deallocates the memory
  * @param stack
  */
-//void destroyStack(Stack_t* stack);
+void destroyStack(Stack_t* stack);
 /**
  * Checks whether the stack is full
  * @param stack
  * @return
  */
-//bool isFull(Stack_t stack);
+bool isFull(Stack_t stack);
 /**
  * Checks whether the stack is empty
  * @param stack
  * @return
  */
-//bool isEmpty(Stack_t stack);
+bool isEmpty(Stack_t stack);
 /**
  * Pushes a new item to the stack,
  * if it is not full
  * @param stack
  * @param item
  */
-//void push(Stack_t* stack, int item);
+void push(Stack_t* stack, int item);
 /**
  * Removes the top element from the stack,
  * if it is not empty
  * @param stack
  * @return
  */
-//int pop(Stack_t* stack);
+int pop(Stack_t* stack);
 /**
  * Returns the top element of the stack,
  * if it is not empty
  * @param stack
  * @return
  */
-//int peek(Stack_t stack);
+int peek(Stack_t stack);
 /**
  * Returns the size of the stack
  * @param stack
  * @return
  */
-//int size(Stack_t stack);
+int size(Stack_t stack);
 
 #endif //STACK_H
