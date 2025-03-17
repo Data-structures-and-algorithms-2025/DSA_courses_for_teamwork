3b4db144a249975d4e4de0ca3679659c38b38ee7
MezofiTihamer <mtihamer123@gmail.com>
2025-03-17 09:40:56 +0200

    Update product.c

--- week-03/operations-with-struct/product/product.c
+++ week-03/operations-with-struct/product/product.c
@@ -9,9 +9,9 @@ void readProductDetails(Product_t *pProduct){
     scanf("%^[\n]\n", pProduct->name);
     scanf("%^[\n]\n", pProduct->barcode);
     scanf("%^[\n]\n", pProduct->supplier);
-    scanf("%d", &pProduct->yearofManufacture.year);
-    scanf("%d", &pProduct->yearofManufacture.month);
-    scanf("%d\n", &pProduct->yearofManufacture.day);
+    scanf("%d", &pProduct->dateOfManufacture.year);
+    scanf("%d", &pProduct->dateOfManufacture.month);
+    scanf("%d\n", &pProduct->dateOfManufacture.day);
     scanf("%d\n", &pProduct->category);
     scanf("%d\n", &pProduct->price);
 }
@@ -20,7 +20,7 @@ void printProduct(Product_t product){
     printf("Product name: %s\n", product.name);
     printf("Product barcode: %s\n", product.barcode);
     printf("Product supplier: %s\n", product.supplier);
-    printf("Date of manufacture: %d - %d - %d\n", product.yearofManufacture.year, product.yearofManufacture.month, product.yearofManufacture.day);
+    printf("Date of manufacture: %d - %d - %d\n", product.dateOfManufacture.year, product.dateOfManufacture.month, product.dateOfManufacture.day);
     printf("Category : %d\n", product.category);
     printf("Price: %d\n", product.price);
 }
