af5a6593bac39890668234cb8395f54afde3edb0
BalazsSapi <domokos.balazs@student.ms.sapientia.ro>
2025-03-17 09:56:55 +0200

    Week-03 stack.c and stack.h done. Not tested! cica (Dávid kevéért)

--- week-03/operations-with-stack/int_stack/stack.c
+++ week-03/operations-with-stack/int_stack/stack.c
@@ -3,3 +3,62 @@
 //
 
 #include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void createStack(int capacity, STACK * stack){
+    stack=malloc(sizeof(STACK));
+    stack->capacity = capacity;
+    stack->items = (int *)malloc(stack->capacity * sizeof(int));
+    stack->top = -1;
+    if(!stack->items){
+      printf("Memory allocation error\n");
+    }
+}
+
+void destroyStack(STACK* stack) {
+    free(stack->items);
+    free(stack);
+}
+
+bool isFull(STACK stack) {
+    if (stack.capacity==stack.top+1) {
+        return true;
+    }
+    return false;
+}
+
+bool isEmpty(STACK stack) {
+    if (stack.top==-1) {
+        return true;
+    }
+    return false;
+}
+
+void push(STACK *stack, int item) {
+    if (!isFull(*stack)) {
+        stack->items[++stack->top] = item;
+    }
+}
+
+int pop(STACK *stack) {
+    int temp=stack->items[stack->top];
+    if (!isEmpty(*stack)) {
+        stack->items[stack->top] = NULL;
+        stack->top--;
+    }
+    return temp;
+}
+
+int peek(STACK stack) {
+    return stack.items[stack.top];
+}
+
+int size(STACK stack) {
+    return stack.top+1;
+}
+
+
+
+
--- week-03/operations-with-stack/int_stack/stack.h
+++ week-03/operations-with-stack/int_stack/stack.h
@@ -8,58 +8,65 @@
 Definiálj egy Stack címkéjű struktúrát, amely egy egész számokat tartalmazó verem információit tárolja a következő mezők révén:
 •	capacity/kapacitás (az elemek maximális száma)
 •	top (a verem teteje – a legfelső elem pozíciója)
-•	items/elemek (egész számokat tartalmazó tömb)
-Adottak a következő stack.h fájlban megadott függvény deklarációk. Implementáld a függvényeket az stack.c fájlban.*/
+•	items/elemek (egész számokat tartalmazó tömb)*/
+
+typedef struct{
+    int capacity;
+    int top;
+    int *items;
+  }STACK;
+
+/*Adottak a következő stack.h fájlban megadott függvény deklarációk. Implementáld a függvényeket az stack.c fájlban.*/
 /**
  * Initializing the capacity and size of stack,
  * allocating memory for the @elements array
  * @param capacity
  * @param stack
  */
-//void createStack(int capacity, Stack_t * stack);
+void createStack(int capacity, STACK * stack);
 /**
  * Deallocates the memory
  * @param stack
  */
-//void destroyStack(Stack_t* stack);
+void destroyStack(STACK* stack);
 /**
  * Checks whether the stack is full
  * @param stack
  * @return
  */
-//bool isFull(Stack_t stack);
+bool isFull(STACK stack);
 /**
  * Checks whether the stack is empty
  * @param stack
  * @return
  */
-//bool isEmpty(Stack_t stack);
+bool isEmpty(STACK stack);
 /**
  * Pushes a new item to the stack,
  * if it is not full
  * @param stack
  * @param item
  */
-//void push(Stack_t* stack, int item);
+void push(STACK* stack, int item);
 /**
  * Removes the top element from the stack,
  * if it is not empty
  * @param stack
- * @return
+ * @return top
  */
-//int pop(Stack_t* stack);
+int pop(STACK* stack);
 /**
  * Returns the top element of the stack,
  * if it is not empty
  * @param stack
  * @return
  */
-//int peek(Stack_t stack);
+int peek(STACK stack);
 /**
  * Returns the size of the stack
  * @param stack
  * @return
  */
-//int size(Stack_t stack);
+int size(STACK stack);
 
 #endif //STACK_H
