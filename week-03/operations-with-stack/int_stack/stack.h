46cd3c6ab2ac211a0b9f5ce086f08f0bcb4d788f
BuksaAron <aronbuksa12@gmail.com>
2025-03-17 09:50:55 +0200

    classwork1

--- week-03/operations-with-stack/int_stack/stack.h
+++ week-03/operations-with-stack/int_stack/stack.h
@@ -16,50 +16,57 @@ Adottak a következő stack.h fájlban megadott függvény deklarációk. Implem
  * @param capacity
  * @param stack
  */
-//void createStack(int capacity, Stack_t * stack);
+
+typedef struct Stack {
+    int capacity;
+    int top;
+    int *items;
+}Stack_t;
+
+void createStack(int capacity, Stack_t * stack);
 /**
  * Deallocates the memory
  * @param stack
  */
-//void destroyStack(Stack_t* stack);
+void destroyStack(Stack_t* stack);
 /**
  * Checks whether the stack is full
  * @param stack
  * @return
  */
-//bool isFull(Stack_t stack);
+bool isFull(Stack_t stack);
 /**
  * Checks whether the stack is empty
  * @param stack
  * @return
  */
-//bool isEmpty(Stack_t stack);
+bool isEmpty(Stack_t stack);
 /**
  * Pushes a new item to the stack,
  * if it is not full
  * @param stack
  * @param item
  */
-//void push(Stack_t* stack, int item);
+void push(Stack_t* stack, int item);
 /**
  * Removes the top element from the stack,
  * if it is not empty
  * @param stack
  * @return
  */
-//int pop(Stack_t* stack);
+int pop(Stack_t* stack);
 /**
  * Returns the top element of the stack,
  * if it is not empty
  * @param stack
  * @return
  */
-//int peek(Stack_t stack);
+int peek(Stack_t stack);
 /**
  * Returns the size of the stack
  * @param stack
  * @return
  */
-//int size(Stack_t stack);
+int size(Stack_t stack);
 
 #endif //STACK_H
