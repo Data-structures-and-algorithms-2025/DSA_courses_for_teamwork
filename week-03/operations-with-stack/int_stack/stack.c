2ab68e4adfff015774de75eb05d25939bfbb879c
Norbinhoo <modinorbert14@gmail.com>
2025-03-17 09:44:58 +0200

    week3

--- week-03/operations-with-stack/int_stack/stack.c
+++ week-03/operations-with-stack/int_stack/stack.c
@@ -3,3 +3,34 @@
 //
 
 #include "stack.h"
+#include <stdlib.h>
+
+void createStack(int capacity, Stack_t * stack){
+      stack=(Stack_t *)malloc(capacity*sizeof(Stack_t));
+}
+void destroyStack(Stack_t* stack){
+    free(stack);
+}
+bool isFull(Stack_t stack){
+     if(size==stack.capacity-1){
+         return true;
+     }
+     else return false;
+}
+bool isEmpty(Stack_t stack){
+      if(size==0){
+        return true;
+      }
+      else return false;
+}
+void push(Stack_t* stack, int item){
+    if(isFull){
+        stack->capacity++;
+        stack->elements[0]=item;
+    }
+}
+int pop(Stack_t* stack){
+     if(isEmpty){
+       *stack->elements
+     }
+}
--- week-03/operations-with-stack/int_stack/stack.h
+++ week-03/operations-with-stack/int_stack/stack.h
@@ -62,4 +62,18 @@ Adottak a következő stack.h fájlban megadott függvény deklarációk. Implem
  */
 //int size(Stack_t stack);
 
+typedef struct{
+    int capacity;
+    int top;
+    int *elements;
+}Stack_t;
+void createStack(int capacity, Stack_t * stack);
+void destroyStack(Stack_t* stack);
+bool isFull(Stack_t stack);
+bool isEmpty(Stack_t stack);
+void push(Stack_t* stack, int item);
+int pop(Stack_t* stack);
+int peek(Stack_t stack);
+int size(Stack_t stack);
+
 #endif //STACK_H
