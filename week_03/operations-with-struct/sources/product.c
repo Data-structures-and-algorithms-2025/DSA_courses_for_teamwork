5892e6896b1c96744eb106ff1e74ace72bcdf8b0
Munlaly <nemetdbence@gmail.com>
2025-03-17 09:17:11 +0200

    Revert "initialization"
    This reverts commit b98b05e77b20d51dcd9543e6181621a4696fedbd.

--- week_03/operations-with-struct/sources/product.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Bence on 3/17/2025.
-//
-
-#include "product.h"
--- week_03/operations-with-struct/sources/product.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Bence on 3/17/2025.
-//
-
-#ifndef PRODUCT_H
-#define PRODUCT_H
-
-#endif //PRODUCT_H
