6fd6d75440bb1a04809e243d76c6c812cee3dcb8
Kovacs1Dani <dk.kovacs123@gmail.com>
2025-03-19 21:55:52 +0200

    stack functions added

--- week-03/operations-with-stack/int_stack/stack.c
+++ week-03/operations-with-stack/int_stack/stack.c
@@ -3,3 +3,63 @@
 //
 
 #include "stack.h"
+#include <stdlib.h>
+#include<stdio.h>
+
+void createStack(int capacity, Stack_t *stack) {
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->items =(int*)malloc(capacity* sizeof(int));
+}
+
+void destroyStack(Stack_t *stack) {
+    stack->capacity = 0;
+    stack->top = -1;
+    free(stack->items);
+    stack = NULL;
+}
+
+bool isFull(Stack_t stack) {
+    return stack.top == stack.capacity -1;
+}
+
+bool isEmpty(Stack_t stack) {
+    return stack.top == -1;
+}
+
+void push(Stack_t *stack, int item) {
+    if(isFull(*stack)) {
+        printf("The stack is full!\n");
+        //exit(-1);
+    }
+    else {
+        stack->items[++stack->top] = item;
+    }
+}
+
+int pop(Stack_t *stack) {
+    if( isEmpty(*stack)) {
+        printf("The stack is empty!\n");
+        return 0;
+    }
+    else {
+        int save = stack->items[stack->top];
+        stack->items[stack->top--] = INT_MIN;
+        return save;
+    }
+}
+
+int peek(Stack_t stack) {
+    if( isEmpty(stack)) {
+        printf("The stack is empty!\n");
+        return 0;
+    }
+    else {
+        return  stack.items[stack.top];
+    }
+}
+
+int size(Stack_t stack) {
+    return stack.top+1;
+}
+
--- week-03/operations-with-stack/int_stack/stack.h
+++ week-03/operations-with-stack/int_stack/stack.h
@@ -4,6 +4,13 @@
 
 #ifndef STACK_H
 #define STACK_H
+
+#include <stdlib.h>
+typedef struct {
+ int capacity;
+ int top;
+ int *items;
+}Stack_t;
 /*Implementáld a veremmel kapcsolatos alapvető műveleteket: create, destroy, push, pop, peek, isFull, isEmpty. Ezeket a műveleteket az stack.h, stack.c és main.c fájlok segítségével valósítsd meg.
 Definiálj egy Stack címkéjű struktúrát, amely egy egész számokat tartalmazó verem információit tárolja a következő mezők révén:
 •	capacity/kapacitás (az elemek maximális száma)
@@ -61,5 +68,56 @@ Adottak a következő stack.h fájlban megadott függvény deklarációk. Implem
  * @return
  */
 //int size(Stack_t stack);
+/**
+* Initializing the capacity and size of stack,
+* allocating memory for the @elements array
+* @param capacity
+* @param stack
+*/
+void createStack(int capacity, Stack_t * stack);
+/**
+* Deallocates the memory
+* @param stack
+*/
+void destroyStack(Stack_t* stack);
+/**
+* Checks whether the stack is full
+* @param stack
+* @return
+*/
+bool isFull(Stack_t stack);
+/**
+* Checks whether the stack is empty
+* @param stack
+* @return
+*/
+bool isEmpty(Stack_t stack);
+/**
+* Pushes a new item to the stack,
+* if it is not full
+* @param stack
+* @param item
+*/
+void push(Stack_t* stack, int item);
+/**
+* Removes the top element from the stack,
+* if it is not empty
+* @param stack
+* @return
+*/
+int pop(Stack_t* stack);
+/**
+* Returns the top element of the stack,
+* if it is not empty
+* @param stack
+* @return
+*/
+int peek(Stack_t stack);
+/**
+* Returns the size of the stack
+* @param stack
+* @return
+*/
+int size(Stack_t stack);
 
 #endif //STACK_H
