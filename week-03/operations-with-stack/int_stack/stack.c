eeb1fd9c2e08afd4497bab0c49d6e49cbc960fb4
KisBenjamin <kis.beniamin.tibor@student.ms.sapientia.ro>
2025-03-21 12:19:02 +0200

    Operations with stack done

--- week-03/operations-with-stack/int_stack/stack.c
+++ week-03/operations-with-stack/int_stack/stack.c
@@ -3,3 +3,73 @@
 //
 
 #include "stack.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void createStack(int capacity, Stack_t * stack)
+{
+    stack->capacity = capacity;
+    stack->top = -1;
+
+    stack->elements =(int*)malloc(capacity*sizeof(int));
+    if (!stack->elements)
+    {
+        printf("Memory allocation failed!");
+        exit(404);
+    }
+   }
+
+void destroyStack(Stack_t* stack)
+{
+    free(stack->elements);
+}
+
+bool isFull(Stack_t stack)
+{
+    if(stack.capacity-1 == stack.top)
+    {
+        return true;
+    }
+    return false;
+}
+bool isEmpty(Stack_t stack)
+{
+    if(stack.top == -1)
+    {
+        return true;
+    }
+    return false;
+}
+
+void push(Stack_t* stack, int item)
+{
+    if (isFull(*stack)) {
+        printf("It's full!\n");
+        return;
+    }
+    stack->elements[++stack->top] = item;
+}
+int pop(Stack_t* stack)
+{
+    if(!isEmpty(*stack))
+    {
+        int value =stack->elements[stack->top];
+        stack->top--;
+        return value;
+    }
+    return -1;
+}
+
+int peek(Stack_t stack)
+{
+    if (isEmpty(stack)) {
+        printf("Stack is empty!\n");
+        return -1;
+    }
+    return stack.elements[stack.top];
+}
+
+int size(Stack_t stack)
+{
+    return stack.top+1;
+}
--- week-03/operations-with-stack/main.c
+++ week-03/operations-with-stack/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "int_stack/stack.h"
+
 int main(void)
 {
     /*Egy kezdetben üres, maximum 4 kapacitású verembe próbáljuk elhelyezni, ebben a sorrendben, az 1,2,3,4,5 számokat.
@@ -10,5 +12,31 @@ int main(void)
     //A vermet nem szabad bejárni!!!
     //A main-ben ne hivatkozz a struktúra mezőire!!!
 
+    Stack_t verem;
+    createStack(4,&verem);
+    for(int i=1;i<=5;i++)
+    {
+        push(&verem,i);
+    }
+    pop(&verem);
+    pop(&verem);
+    for(int i=6;i<=8;i++)
+    {
+        push(&verem,i);
+    }
+    pop(&verem);
+
+    int top = peek(verem);
+    printf("Felso elem a muveletek utan: %d \n",top);
+
+    int sum =0;
+
+    while(!isEmpty(verem))
+    {
+        sum += pop(&verem);
+    }
+    printf("A veremben talalhato elemek ossz: %d",sum);
+
+    destroyStack(&verem);
     return 0;
 }
