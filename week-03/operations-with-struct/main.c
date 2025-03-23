7c467f7b68717978fadbcfa98e0ccd3593f8d2f4
BenczeMihaly <benczemihaly6@gmail.com>
2025-03-23 19:53:59 +0200

    Update main.c

--- week-03/operations-with-struct/main.c
+++ week-03/operations-with-struct/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "product/product.h"
+
 int main(void)
 {
     /*1.	Hozz létre egy terméket, melynek a deklarálás pillanatában már adj kezdőértékeket. Minden mezőt tölts fel adattal.
@@ -7,6 +9,25 @@ int main(void)
     2.	Hozz létre egy terméket, melynek adatait olvasd be állományból (readProductDetails).
     Írd ki a termék adatait a képernyőre (printProduct).
     */
+
+    Product_t product1 = {
+        "Tej 1L",
+        "1234567890",
+        "Milka Kft.",
+        {2024, 3, 10},
+        DIARY,
+        399.99
+    };
+
+    printf("Elso termek adatai:\n");
+    printProduct(product1);
+
+    Product_t product2;
+    printf("\nMasodik termek beolvasasa:\n");
+    readProductDetails(&product2);
+
+    printf("\nMasodik termek adatai:\n");
+    printProduct(product2);
 
     return 0;
 }
