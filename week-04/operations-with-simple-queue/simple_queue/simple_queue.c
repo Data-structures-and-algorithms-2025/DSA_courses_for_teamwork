29189a76e30012eb15e509a83240dbbbc050ea0f
Norbinhoo <modinorbert14@gmail.com>
2025-03-24 09:37:40 +0200

    simplequeue

--- week-04/operations-with-simple-queue/simple_queue/simple_queue.c
+++ week-04/operations-with-simple-queue/simple_queue/simple_queue.c
@@ -3,3 +3,75 @@
 //
 
 #include "simple_queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void createQueue(int capacity, Simple_Queue_t* queue)
+{
+    queue->capacity=capacity;
+    queue->rear=queue->front=-1;
+    queue->elements=(int*)malloc(capacity*sizeof(int));
+    if (!queue->elements)
+    {
+        printf("Memory allocation error");
+        exit(-1);
+    }
+}
+void destroyQueue(Simple_Queue_t* queue){
+    free(queue->elements);
+}
+bool isFull(Simple_Queue_t queue)
+{
+    return queue.rear==queue.capacity-1;
+}
+bool isEmpty(Simple_Queue_t queue)
+{
+    return queue.front == -1 || queue.front > queue.rear;
+}
+void enqueue(Simple_Queue_t* queue, int item) {
+    if (isFull(*queue)) {
+        printf("The queue is full\n");
+        exit(-3);
+    }
+    if (isEmpty(*queue)) {
+        queue->front = 0;
+    }
+    queue->rear++;
+    queue->elements[queue->rear] = item;
+}
+int dequeue(Simple_Queue_t* queue)
+{
+    if (isEmpty(*queue))
+    {
+        printf("The queue is empty");
+        exit(-3);
+    }
+    int save=queue->elements[queue->front];
+    if (queue->front==queue->rear)
+        queue->front=queue->rear=-1;
+    else
+        queue->front++;
+    return save;
+}
+void display(Simple_Queue_t queue)
+{
+    if (isEmpty(queue))
+    {
+        printf("The queue is empty");
+        exit(-3);
+    }
+    for (int i=queue.front;i<=queue.rear;i++)
+    {
+        printf("%i ",queue.elements[i]);
+    }
+    printf("\n");
+}
+
+int peek(Simple_Queue_t queue)
+{if (isEmpty(queue))
+{
+    printf("The queue is empty");
+    exit(-3);
+}
+    return queue.elements[queue.front];
+}
