73b50b1725fb8e8a0b631bd533fa60ce805eb2b0
zolcseakdora <zolcseak.dora@student.ms.sapientia.ro>
2025-03-24 09:25:28 +0200

    a fuggvenyek definicioja

--- week-04/operations-with-simple-queue/simple_queue/simple_queue.c
+++ week-04/operations-with-simple-queue/simple_queue/simple_queue.c
@@ -15,3 +15,65 @@ void createQueue(int capacity, Simple_Queue_t* queue)
         exit(-1);
     }
 }
+void destroyQueue(Simple_Queue_t* queue){
+    free(queue->elements);
+    queue->elements = NULL;
+    queue->front = queue->rear = -1;
+    queue->capacity = 0;
+}
+bool isFull(Simple_Queue_t queue)
+{
+    return queue.rear==queue.capacity - 1;
+}
+bool isEmpty(Simple_Queue_t queue)
+{
+     return queue.front==-1;
+}
+void enqueue(Simple_Queue_t* queue, int item)
+{
+    if (isFull(*queue))
+    {
+        printf("The stack is full");
+        exit(-3);
+    }
+    if (isEmpty(*queue))
+    {
+        queue->front==0;
+    }
+    queue->elements[++queue->rear]=item;
+}
+int dequeue(Simple_Queue_t* queue)
+{
+    if (isEmpty(*queue))
+    {
+        printf("The queue is empty");
+        exit(-3);
+    }
+    int save=queue->elements[queue->front];
+    if (queue->front==queue->rear)
+        queue->front=queue->rear=-1;
+    else
+        queue->front++;
+    return save;
+}
+void display(Simple_Queue_t queue)
+{
+    if (isEmpty(queue))
+    {
+        printf("The queue is empty");
+        exit(-3);
+    }
+    for (int i=queue.front;i<=queue.rear;i++)
+    {
+        printf("%i ",queue.elements[i]);
+    }
+    printf("\n");
+}
+int peek(Simple_Queue_t queue)
+{    if (isEmpty(queue))
+        {
+            printf("The queue is empty");
+            exit(-3);
+        }
+    return queue.elements[queue.front
+}
