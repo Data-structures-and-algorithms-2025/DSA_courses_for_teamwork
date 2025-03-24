ef6f80d45c4d74aa3efdbfad9334672a5ebf7302
AttilaHegyeli <hegyeli.attila@student.ms.sapientia.ro>
2025-03-24 09:52:02 +0200

    week04-circular-isFull-Locate-deLocate

--- week-04/operations-with-circular-queue/circular_queue/circular_queue.c
+++ week-04/operations-with-circular-queue/circular_queue/circular_queue.c
@@ -2,4 +2,27 @@
 // Created by oszti on 3/23/2025.
 //
 
+#include <malloc.h>
 #include "circular_queue.h"
+
+void createQueue(int capacity, CircularQueue_t *queue) {
+    queue->capacity = capacity;
+    queue->rear = -1;
+    queue->front = -1;
+    queue->elements = (int *) malloc(capacity * sizeof(int));
+    if (queue->elements == NULL) {
+        perror("Mermoy!");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void destroyQueue(CircularQueue_t *queue) {
+    free(queue->elements);
+    free(queue);
+    queue = NULL;
+}
+
+bool isFull(CircularQueue_t queue)
+{
+    return ((queue.rear + 1) % queue.capacity) == queue.front;
+}
