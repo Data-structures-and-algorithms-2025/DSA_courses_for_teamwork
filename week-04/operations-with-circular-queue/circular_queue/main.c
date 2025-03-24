c80ff4d43e528f80c2001b96411fcace9f1b401b
Idegenmouse <rig.david.v@gmail.com>
2025-03-24 09:41:48 +0200

    Update main.c

--- week-04/operations-with-circular-queue/circular_queue/main.c
+++ week-04/operations-with-circular-queue/circular_queue/main.c
@@ -29,7 +29,7 @@ int main(void)
 
     dequeue(&queue); //egy ertek kivetele
 
-    printf("%d",peek(queue)); //sor elejenek kiiratasa
+    printf("%d/n",peek(queue)); //sor elejenek kiiratasa
 
 
     //display(queue); //kiiratjuk a sort ellenorzes celjabol
@@ -37,7 +37,7 @@ int main(void)
     int temp = 0;
     for (int i = 0; i < queue.capacity; ++i) {
         temp += peek(queue);
-        dequeue(&queue);
+        //dequeue(&queue);
     }
     printf("a sorban levo elemek osszege: %d",temp);
 
