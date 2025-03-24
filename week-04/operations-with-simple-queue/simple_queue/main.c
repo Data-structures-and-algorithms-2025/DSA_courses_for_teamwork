98161d633a3868b35ca683aece627bf651f0b3c2
Domokos Botond <domokos.botond@student.ms.sapientia.ro>
2025-03-24 09:28:39 +0200

    megy

---
+++ week-04/operations-with-simple-queue/simple_queue/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+1.	Egy kezdetben üres, maximum 5 kapacitású sorba próbáljuk elhelyezni, ebben a sorrendben, a 10, 20, 30, 40, 50 számokat.
+2.	Vegyünk ki két értéket, majd próbáljuk betenni, ebben a sorrendben, a 60, 70 és 80 számokat.
+3.	Utána vegyünk ki 1 értéket.
+4.	Melyik szám lesz a sor elején a leírt műveletek elvégzése után?
+5.  Mennyi lesz a sorban található elemek összege a leírt műveletek elvégzése után?
+Megjegyzes: A sort nem szabad bejárni, csak a megfelelő műveleteket használhatod.*/
+
+    return 0;
+}
