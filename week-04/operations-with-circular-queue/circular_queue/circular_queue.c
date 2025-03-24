1bce21818ddda1bb4456df21ea2d6beb9895d812
Norbinhoo <modinorbert14@gmail.com>
2025-03-24 09:48:41 +0200

    gyak4

--- week-04/operations-with-circular-queue/circular_queue/circular_queue.c
+++ week-04/operations-with-circular-queue/circular_queue/circular_queue.c
@@ -4,4 +4,72 @@
 
 #include "circular_queue.h"
 
+void createQueue(int capacity, CircularQueue_t* queue){
+      
+}
+void destroyQueue(CircularQueue_t* queue){
 
+}
+bool isFull(CircularQueue_t queue)
+{
+    return (queue.front-1==queue.rear || (queue.front==0 &&
+   queue.rear==queue.capacity-1));
+}
+bool isEmpty(CircularQueue_t queue)
+{
+ return queue.front==-1;
+}
+void enqueue(CircularQueue_t* queue, int item)
+{
+ if (isFull(*queue))
+ {
+ printf("It is full");
+ exit(-3);
+ }
+ if (isEmpty(*queue))
+ {
+ queue->front=0;
+ }
+ queue->rear=(queue->rear+1)%queue->capacity;
+ queue->elements[]=item;
+}
+int dequeue(CircularQueue_t* queue)
+{
+ if (isEmpty(*queue))
+ {
+ printf("is empty");
+ return INT_MIN;
+ }int save=queue->elements[queue->front];
+ if (queue->front==queue->rear)
+ {
+ queue->front=queue->rear=-1;
+ }
+ else
+ {
+ queue->front=(queue->front+1)%queue->capacity;
+ }
+ return …;
+}
+void display(CircularQueue_t queue)
+{
+ if (isEmpty(queue))
+ {
+ printf("is empty");
+ exit(-4);
+ }
+ if (queue.front==queue.rear)
+ printf("%i ",queue.elements[queue.front]);
+ else
+ {
+ int i=queue.front;
+ do
+ {
+ printf("%i ",queue.elements[i]);
+ i=(…)%queue.capacity;
+ }
+ while (i!=queue.rear);
+ printf("%i ",queue.elements[i]);
+ }
+ printf("\n");
+}
+int peek(CircularQueue_t queue)
