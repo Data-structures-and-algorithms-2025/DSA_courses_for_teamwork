3cf611b51f5fe1f156fbf7807243139e2a9d705a
kovacsnora <kovacs.nora@student.ms.sapientia.ro>
2025-03-24 09:47:33 +0200

    simple_queue.c  ujabb

--- week-04/operations-with-simple-queue/simple_queue/simple_queue.c
+++ week-04/operations-with-simple-queue/simple_queue/simple_queue.c
@@ -4,76 +4,77 @@
 
 #include "simple_queue.h"
 #include <stdlib.h>
-void createQueue(int capacity, Simple_Queue_t* queue)
-{
-    queue->capacity=capacity;
-    queue->rear=queue->front=-1;
-    queue->elements=(int*)malloc(capacity*sizeof(int));
-    if (!queue->elements)
-    {
-        printf("Memory allocation error");
+#include <stdio.h>
+void createQueue(int capacity, Simple_Queue_t* queue) {
+    queue->capacity = capacity;
+    queue->front = queue->rear = -1;
+    queue->elements = (int*)malloc(capacity * sizeof(int));
+    if (!queue->elements) {
+        printf("Memory allocation error\n");
         exit(-1);
     }
 }
-void destroyQueue(Simple_Queue_t* queue){
+
+void destroyQueue(Simple_Queue_t* queue) {
     free(queue->elements);
     queue->elements = NULL;
     queue->front = queue->rear = -1;
     queue->capacity = 0;
 }
-bool isFull(Simple_Queue_t queue)
-{
-    return queue.rear=queue.capacity - 1;
+
+bool isFull(Simple_Queue_t queue) {
+    return (queue.rear + 1) % queue.capacity == queue.front;
 }
-bool isEmpty(Simple_Queue_t queue)
-{
-     return queue.front=-1;
+
+bool isEmpty(Simple_Queue_t queue) {
+    return queue.front == -1;
 }
-void enqueue(Simple_Queue_t* queue, int item)
-{
-    if (isFull(*queue))
-    {
-        printf("The stack is full");
+
+void enqueue(Simple_Queue_t* queue, int item) {
+    if (isFull(*queue)) {
+        printf("The queue is full\n");
+        return;
+    }
+    if (isEmpty(*queue)) {
+        queue->front = queue->rear = 0;
+    } else {
+        queue->rear = (queue->rear + 1) % queue->capacity;
+    }
+    queue->elements[queue->rear] = item;
+}
+
+int dequeue(Simple_Queue_t* queue) {
+    if (isEmpty(*queue)) {
+        printf("The queue is empty\n");
         exit(-3);
     }
-    if (isEmpty(*queue))
-    {
-        queue->front=0;
+    int save = queue->elements[queue->front];
+    if (queue->front == queue->rear) {
+        queue->front = queue->rear = -1;
+    } else {
+        queue->front = (queue->front + 1) % queue->capacity;
     }
-    queue->elements[++queue->rear]=item;
-}
-int dequeue(Simple_Queue_t* queue)
-{
-    if (isEmpty(*queue))
-    {
-        printf("The queue is empty");
-        exit(-3);
-    }
-    int save=queue->elements[queue->front];
-    if (queue->front==queue->rear)
-        queue->front=queue->rear=-1;
-    else
-        queue->front++;
     return save;
 }
-void display(Simple_Queue_t queue)
-{
-    if (isEmpty(queue))
-    {
-        printf("The queue is empty");
-        exit(-3);
+
+void display(Simple_Queue_t queue) {
+    if (isEmpty(queue)) {
+        printf("The queue is empty\n");
+        return;
     }
-    for (int i=queue.front;i<=queue.rear;i++)
-    {
-        printf("%i ",queue.elements[i]);
+    int i = queue.front;
+    while (true) {
+        printf("%d ", queue.elements[i]);
+        if (i == queue.rear) break;
+        i = (i + 1) % queue.capacity;
     }
     printf("\n");
 }
-int peek(Simple_Queue_t queue)
-{    if (isEmpty(queue))
-        {
-            printf("The queue is empty");
-            exit(-3);
-        }
+
+int peek(Simple_Queue_t queue) {
+    if (isEmpty(queue)) {
+        printf("The queue is empty\n");
+        exit(-3);
+    }
     return queue.elements[queue.front];
 }
