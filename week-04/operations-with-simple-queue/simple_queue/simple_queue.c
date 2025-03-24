061608154d1b8d279811af5f805d9b2edb131610
GyorgySzabi <gyorgy.szabolcs@student.ms.sapientia.ro>
2025-03-24 09:49:35 +0200

    Update simple_queue.c
    simple_queue.c done

--- week-04/operations-with-simple-queue/simple_queue/simple_queue.c
+++ week-04/operations-with-simple-queue/simple_queue/simple_queue.c
@@ -3,3 +3,74 @@
 //
 
 #include "simple_queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void createQueue(int capacity, Simple_Queue_t* queue) {
+    queue->capacity = capacity;
+    queue->rear = queue->front = -1;
+    queue->elements = (int*)malloc(capacity * sizeof(int));
+    if (!queue->elements) {
+        printf("Memory allocation error\n");
+        exit(-1);
+    }
+}
+
+void destroyQueue(Simple_Queue_t* queue) {
+    free(queue->elements);
+    queue->elements = NULL;
+    queue->front = queue->rear = -1;
+    queue->capacity = 0;
+}
+
+bool isFull(Simple_Queue_t queue) {
+    return queue.rear == queue.capacity - 1;
+}
+
+bool isEmpty(Simple_Queue_t queue) {
+    return queue.front == -1;
+}
+
+void enqueue(Simple_Queue_t* queue, int item) {
+    if (isFull(*queue)) {
+        printf("The queue is full\n");
+        return;
+    }
+    if (isEmpty(*queue)) {
+        queue->front = queue->rear = 0;
+    }
+    queue->elements[queue->rear++] = item;
+}
+
+int dequeue(Simple_Queue_t* queue) {
+    if (isEmpty(*queue)) {
+        printf("The queue is empty\n");
+        return -1;
+    }
+    int save = queue->elements[queue->front];
+    if (queue->front == queue->rear) {
+        queue->front = queue->rear = -1;
+    } else {
+        queue->front++;
+    }
+    return save;
+}
+
+void display(Simple_Queue_t queue) {
+    if (isEmpty(queue)) {
+        printf("The queue is empty\n");
+        return;
+    }
+    for (int i = queue.front; i <= queue.rear; i++) {
+        printf("%i ", queue.elements[i]);
+    }
+    printf("\n");
+}
+
+int peek(Simple_Queue_t queue) {
+    if (isEmpty(queue)) {
+        printf("The queue is empty\n");
+        return -1;
+    }
+    return queue.elements[queue.front];
+}
