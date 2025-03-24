b6fd24e46b3d3d884730e1d551bbd0bbb7a4fde6
BuksaAron <aronbuksa12@gmail.com>
2025-03-24 08:35:37 +0200

    teamwork

--- week-03/operations-with-stack/main.c
+++ week-03/operations-with-stack/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "int_stack/stack.h"
+
 int main(void)
 {
     /*Egy kezdetben üres, maximum 4 kapacitású verembe próbáljuk elhelyezni, ebben a sorrendben, az 1,2,3,4,5 számokat.
@@ -9,6 +11,27 @@ int main(void)
     d) Mennyi lesz a veremben található elemek összege a leírt műveletek elvégzése után?*/
     //A vermet nem szabad bejárni!!!
     //A main-ben ne hivatkozz a struktúra mezőire!!!
+
+    Stack_t stack;
+    createStack(4, &stack);
+    push(&stack, 1);
+    push(&stack, 2);
+    push(&stack, 3);
+    push(&stack, 4);
+    push(&stack, 5);
+    pop(&stack);
+    pop(&stack);
+    push(&stack, 6);
+    push(&stack, 7);
+    push(&stack, 8);
+    pop(&stack);
+    printf("A verem tetejen a %i szam van.\n", peek(stack));
+    int count = 0;
+    while (!isEmpty(stack)) {
+        count += peek(stack);
+        pop(&stack);
+    }
+    printf("A stackben levo elemek oszzege: %i", count);
 
     return 0;
 }
