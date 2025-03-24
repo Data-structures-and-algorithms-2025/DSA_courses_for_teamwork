6b9cba3bab6f2374bed27f4badb9fee378242eb7
MezofiTihamer <mtihamer123@gmail.com>
2025-03-24 09:36:53 +0200

    Update main.c

--- week-04/operations-with-circular-queue/circular_queue/main.c
+++ week-04/operations-with-circular-queue/circular_queue/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "circular_queue.h"
+
 int main(void)
 {
     /*
@@ -9,6 +11,31 @@ int main(void)
  4.	Melyik szám lesz a sor elején a leírt műveletek elvégzése után?
  5.	Mennyi lesz a sorban található elemek összege a leírt műveletek elvégzése után?
  Megjegyzes: A sort nem szabad bejárni, csak a megfelelő műveleteket használhatod!*/
+
+    CircularQueue_t queue;
+
+    createQueue(&queue, 4);
+    enqueue(&queue, 1);
+    enqueue(&queue, 2);
+    enqueue(&queue, 3);
+    enqueue(&queue, 4);
+
+    dequeue(&queue);
+    dequeue(&queue);
+
+    enqueue(&queue, 5);
+    enqueue(&queue, 6);
+    enqueue(&queue, 7);
+
+    printf("The first element after the modifications: %d !", peek(queue));
+
+    int sum = 0;
+    while (!isEmpty(queue)) {
+        sum += peek(queue);
+        dequeue(&queue);
+    }
+
+    destroyQueue(&queue);
 
     return 0;
 }
