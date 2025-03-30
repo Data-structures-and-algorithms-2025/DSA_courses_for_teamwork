1e6d04f66e639f3dbba22daf10bb3ff83c5f3da2
Rory <rory.elllaa@gmail.com>
2025-03-30 21:43:46 +0300

    week-04: simple queue mostly works

--- week-04/operations-with-simple-queue/simple_queue/main.c
+++ week-04/operations-with-simple-queue/simple_queue/main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 
+#include "simple_queue.h"
+
 int main(void)
 {
-    /*
+  /*
 1.	Egy kezdetben üres, maximum 5 kapacitású sorba próbáljuk elhelyezni, ebben a sorrendben, a 10, 20, 30, 40, 50 számokat.
 2.	Vegyünk ki két értéket, majd próbáljuk betenni, ebben a sorrendben, a 60, 70 és 80 számokat.
 3.	Utána vegyünk ki 1 értéket.
 4.	Melyik szám lesz a sor elején a leírt műveletek elvégzése után?
 5.  Mennyi lesz a sorban található elemek összege a leírt műveletek elvégzése után?
 Megjegyzes: A sort nem szabad bejárni, csak a megfelelő műveleteket használhatod.*/
-
-    return 0;
+// 1.
+  Simple_Queue_t queue;
+  int capacity=5;
+  createQueue(capacity, &queue);
+  for (int i = 0; i < capacity; i++) {
+      enqueue(&queue, (i+1)*10);
+  }
+  display(queue);
+// 2.
+  printf("dequeued: %d, %d\n", dequeue(&queue), dequeue(&queue));
+  for (int i = 6; i < 8; i++) {
+      enqueue(&queue, (i)*10);
+  }
+// 3.
+  printf("dequeued: %d\n", dequeue(&queue));
+// 4.
+  printf("peek front: %d\n",peek(queue));
+  display(queue);
+// 5.
+  int sum = 0;
+  for (int i = 0; i < 2; i++) {
+      sum=sum+dequeue(&queue);
+  }
+  printf("Sum is %d\n", sum);
+  return 0;
 }
--- week-04/operations-with-simple-queue/simple_queue/simple_queue.h
+++ week-04/operations-with-simple-queue/simple_queue/simple_queue.h
@@ -1,61 +1,77 @@
 //
 // Created by oszti on 3/23/2025.
 //
+#include <stdbool.h>
+#include <stdio.h>
 
 #ifndef SIMPLE_QUEUE_H
 #define SIMPLE_QUEUE_H
-typedef struct {
-    int capacity;
-    int front;
-    int rear;
-    int *elements;
-}Simple_Queue_t;
+typedef struct
+{
+        int capacity;
+        int front;
+        int rear;
+        int *elements;
+} Simple_Queue_t;
 
 /**
  * Creates the queue data structure, allocates memory for elements
  * @param capacity
  * @param queue
  */
-void createQueue(int capacity, Simple_Queue_t *queue);
+void createQueue (int capacity, Simple_Queue_t *queue);
 /**
  * Deallocates the elements array -> free
  * @param queue
  */
-void destroyQueue(Simple_Queue_t* queue);
+void destroyQueue (Simple_Queue_t *queue);
 /**
  * Checks whether the queue is full or not
  * @param queue
  * @return true/false
  */
-bool isFull(Simple_Queue_t queue);
+bool isFull (Simple_Queue_t queue);
 /**
  * Checks whether the queue is empty or not
  * @param queue
  * @return true/false
  */
-bool isEmpty(Simple_Queue_t queue);
+bool isEmpty (Simple_Queue_t queue);
 /**
  * Adds a new item to the end of the queue
  * @param queue
  * @param item
  */
-void enqueue(Simple_Queue_t* queue, int item);
+void enqueue (Simple_Queue_t *queue, int item);
 /**
  * Removes the first item from the queue
  * @param queue
  * @return
  */
-int dequeue(Simple_Queue_t* queue);
+int dequeue (Simple_Queue_t *queue);
 /**
  * Prints all items of the queue
  * @param queue
  */
-void display(Simple_Queue_t queue);
+void display (Simple_Queue_t queue);
 /**
  * Returns the element in the front
  * @param queue
  * @return the element in the front
  */
-int peek(Simple_Queue_t queue);
+int peek (Simple_Queue_t queue);
 
-#endif //SIMPLE_QUEUE_H
+/**
+ * Returns the element in the rear
+ * @param queue
+ * @return the element in the rear
+ */
+int peekRear (Simple_Queue_t queue);
+
+/**
+ * Returns the number of elements in the queue
+ * @param queue
+ * @return the number of elements in the queue
+ */
+int size (Simple_Queue_t queue);
+#endif // SIMPLE_QUEUE_H
