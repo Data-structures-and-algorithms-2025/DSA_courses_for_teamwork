f69c47154453b9c781b55d4eb6a712c82fccadda
osztianerika <64527127+osztianerika@users.noreply.github.com>
2025-03-30 22:02:21 +0300

    week-05 list exercises template prepared - ready to solve

---
+++ week-05/operations-with-double-list/double_list/double_list_functions.c
@@ -0,0 +1,5 @@
+//
+// Created by oszti on 3/30/2025.
+//
+
+#include "double_list_functions.h"
---
+++ week-05/operations-with-double-list/double_list/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*Írj egy programot, amely az alábbi lépéseket hajtja végre az egyszeresen láncolt listán:
+
+Hozz létre egy üres listát.
+
+Adj hozzá három elemet a lista végéhez a insertAtEnd függvény segítségével.
+
+Adj hozzá egy elemet a lista elejére a insertAtBeginning függvénnyel.
+
+Adj hozzá egy elemet egy adott elem után a insertAfter függvény használatával (például a második elem után).
+
+Ellenőrizd, hogy a lista üres-e az isEmpty függvénnyel, és írd ki az eredményt.
+
+Írd ki a lista elemeit mindkét irányból.*/
+}
