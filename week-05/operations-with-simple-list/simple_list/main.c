8d06b46906f5521604703f63068f68c99c5a2265
B2-T2 <balazs.tamas@student.ms.sapientia.ro>
2025-03-31 09:50:54 +0300

    .

--- week-05/operations-with-simple-list/simple_list/main.c
+++ week-05/operations-with-simple-list/simple_list/main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include "simple_list_functions.h"
 
 int main(void)
 {
-    /*Írj egy programot, amely az alábbi lépéseket hajtja végre az egyszeresen láncolt listán:
+    int data=5;
+    //Hozz létre egy üres listát.
+    int *d = newNode(data);
+    // hozzá három elemet a lista végéhez a insertAtEnd függvény segítségével.
+    insertAtEnd(&d, data);
+    //Adj hozzá egy elemet a lista elejére a insertAtBeginning függvénnyel.
 
-    Hozz létre egy üres listát.
+    //Adj hozzá egy elemet egy adott elem után a insertAfter függvény használatával (például a második elem után).
 
-    Adj hozzá három elemet a lista végéhez a insertAtEnd függvény segítségével.
+    //Ellenőrizd, hogy a lista üres-e az isEmpty függvénnyel, és írd ki az eredményt.
 
-    Adj hozzá egy elemet a lista elejére a insertAtBeginning függvénnyel.
-
-    Adj hozzá egy elemet egy adott elem után a insertAfter függvény használatával (például a második elem után).
-
-    Ellenőrizd, hogy a lista üres-e az isEmpty függvénnyel, és írd ki az eredményt.
-
-    Írd ki a lista elemeit a printList függvénnyel.*/
+    //Írd ki a lista elemeit a printList függvénnyel.
+    
     return 0;
 }
