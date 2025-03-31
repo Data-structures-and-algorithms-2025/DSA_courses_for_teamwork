65f4ce6195f35ab6dc0fcc5370d7f69a1ed62797
B2-T2 <balazs.tamas@student.ms.sapientia.ro>
2025-03-31 09:30:27 +0300

    New Node - Done

--- week-05/operations-with-simple-list/simple_list/simple_list_functions.c
+++ week-05/operations-with-simple-list/simple_list/simple_list_functions.c
@@ -1,5 +1,17 @@
 //
 // Created by oszti on 3/30/2025.
 //
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "simple_list_functions.h"
+
+Node* newNode(int new_data){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if(!new_node) {
+        printf("ERROR");
+        exit(-1);
+    }
+    new_node->data = new_data;
+    new_node->next = NULL;
+    return new_node;
+}
