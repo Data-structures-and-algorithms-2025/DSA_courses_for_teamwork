e6e256c395f1ddced8bbc7dc7c0cbaf8db083eab
B2-T2 <balazs.tamas@student.ms.sapientia.ro>
2025-03-31 09:35:23 +0300

    Insert at beginning - Done

--- week-05/operations-with-simple-list/simple_list/simple_list_functions.c
+++ week-05/operations-with-simple-list/simple_list/simple_list_functions.c
@@ -15,3 +15,16 @@ Node* newNode(int new_data){
     new_node->next = NULL;
     return new_node;
 }
+
+bool isEmpty(Node *head){
+    if(head == NULL){return true;}
+    return false;
+}
+
+void insertAtBeginning(Node** head_ref, int new_data){
+    // Allocate memory to a node
+    Node* new_node = newNode(new_data);
+    new_node->next = (*head_ref);
+    // Move head to new node
+    (*head_ref) = new_node;
+}
