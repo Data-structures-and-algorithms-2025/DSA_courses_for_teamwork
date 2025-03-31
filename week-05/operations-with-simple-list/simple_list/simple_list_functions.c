caf1d088d340136141839da98505af380095bf4e
Norbinhoo <modinorbert14@gmail.com>
2025-03-31 09:52:28 +0300

    simplelist

--- week-05/operations-with-simple-list/simple_list/simple_list_functions.c
+++ week-05/operations-with-simple-list/simple_list/simple_list_functions.c
@@ -3,3 +3,27 @@
 //
 
 #include "simple_list_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Node* newNode(int new_data){
+       Node* node = (Node*)malloc(sizeof(Node));
+    if (node == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    node->data = new_data;
+    node->next = NULL;
+    return node;
+}
+bool isEmpty(Node *head){
+      return head==NULL;
+}
+void insertAtBeginning(Node** head_ref, int new_data){
+    Node* new_node = newNode(new_data);
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+void insertAfter(Node* prev_ref, int new_data){
+
+}
