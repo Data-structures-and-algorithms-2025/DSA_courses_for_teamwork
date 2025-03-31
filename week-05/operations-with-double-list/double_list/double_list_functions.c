6949798bb60c3ff3b44e52f07d81804253ec8658
Csancsesz <vancsa04@gmail.com>
2025-03-31 09:50:18 +0300

    malloc baj van

--- week-05/operations-with-double-list/double_list/double_list_functions.c
+++ week-05/operations-with-double-list/double_list/double_list_functions.c
@@ -3,3 +3,15 @@
 //
 
 #include "double_list_functions.h"
+#include <stdlib.h>
+
+Node* newNode(int new_data){
+    Node uj=(*Node)malloc(sizeof(Node));
+    if(!uj) {
+        perror("malloc baj van");
+        exit(1);
+    }
+    uj->data=new_data;
+    uj->next=NULL;
+    return uj;
+}
