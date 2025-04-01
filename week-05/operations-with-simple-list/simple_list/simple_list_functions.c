f80a9309a068f796dae80c0ccfe1cbff53198c60
TothArminTamas <toth.armin.tamas@student.ms.sapientia.ro>
2025-04-01 14:43:06 +0300

    Update simple_list_functions.c

--- week-05/operations-with-simple-list/simple_list/simple_list_functions.c
+++ week-05/operations-with-simple-list/simple_list/simple_list_functions.c
@@ -21,35 +21,35 @@ bool isEmpty(Node *head) {
     return head == NULL;
 }
 void insertAtBeginning(Node** head_ref, int new_data) {
-Node* new_node = newNode(new_data);
-new_node->next = (*head_ref);
-(*head_ref) = new_node;
+    Node* new_node = newNode(new_data);
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
 }
 void insertAfter(Node* prev_node, int new_data) {
     if (prev_node == NULL) {
     printf("THe given pointer is a NULL pointer");
     return;
 }
-Node* new_node = newNode(new_data);
-new_node->next = prev_node->next;
-prev_node->next = new_node;
+    Node* new_node = newNode(new_data);
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
 }
 
 void insertAtEnd(Node** head_ref, int new_data) {
-Node* new_node = newNode(new_data);
-Node* last = *head_ref;
-if (isEmpty(*head_ref)) {
-*head_ref = new_node;
-return;
+    Node* new_node = newNode(new_data);
+    Node* last = *head_ref;
+    if (isEmpty(*head_ref)) {
+        *head_ref = new_node;
+        return;
 }
-while (last->next != NULL) last = last->next;
-last->next = new_node;
+    while (last->next != NULL) last = last->next;
+    last->next = new_node;
 }
 void printList(Node* node) {
-while (node != NULL) {
-printf("%4d ", node->data);
-node = node->next;
-}
+    while (node != NULL) {
+        printf("%4d ", node->data);
+        node = node->next;
+    }
 }
 
 
