022d6917398245a7d8cc042147829562a11dd2b2
osztianerika <64527127+osztianerika@users.noreply.github.com>
2025-05-11 13:05:14 +0300

    operations-with hashTable

---
+++ week-12/operations-with-hashTable/hash_static_uj/hash_table.c
@@ -0,0 +1,104 @@
+//
+// Created by palma on 5/8/2023.
+//
+
+
+
+#include "hash_table.h"
+
+int hashCode(int key) {
+    return key % CAPACITY;
+}
+
+HashItem createHashItem(int key, int data)
+{
+    HashItem item = {};//TODO
+    return item;
+}
+void createHashArray(HashTable *pHashTable) {
+    pHashTable->items = (HashItem*)calloc(1, sizeof(HashItem));//TODO
+    if(!pHashTable->items)
+    {
+        printf(MEMORY_ALLOCATION_ERROR_MESSAGE);
+        exit(MEMORY_ALLOCATION_ERROR_CODE);
+    }
+    for (int i = 0; i < CAPACITY; ++i) {
+        {};//TODO
+    }
+    pHashTable->size = 0;
+}
+
+void insert(HashTable *hashTable, int key, int data) {
+    if(hashTable->size >= CAPACITY) return;
+    int index = 0;//TODO
+    int i = 0;
+    while (hashTable->items[(index + i)%CAPACITY].key != dummyData.key)
+    {
+        {};//TODO
+    }
+    hashTable->items[(index+i)%CAPACITY] = createHashItem(key, data);
+    hashTable->size++;
+}
+
+void display(HashTable hashTable) {
+    if(0) {//TODO
+        printf("The table is empty\n");
+        return;
+    }
+    printf("The hash table:\n\tpos\t:\tkey\tvalue\t->\tmod\n");
+    for (int i = 0; i < CAPACITY; ++i) {
+        if(hashTable.items[i].key != dummyData.key)
+        {
+            printf("\t%i\t:\t%i\t%i\t->\t%i\n", i, hashTable.items[i].key, hashTable.items[i].data, hashCode(hashTable.items[i].key));
+        }
+    }
+}
+
+int size(HashTable hashTable) {
+    return hashTable.size;
+}
+
+void delete(HashTable *hashTable, int key) {
+    int index = hashCode(key);
+    if (hashTable->items[index].key == dummyData.key)
+    {
+        printf("\n This key does not exist \n");
+        return;
+    }
+    int i = 0;
+    while(hashTable->items[(index+i)%CAPACITY].key != key && i < CAPACITY)
+    {
+        i++;
+    }
+    if(i == CAPACITY)
+    {
+        printf("\n This key does not exist \n");
+        return;//TODO
+    }
+    hashTable->items[(index+i)%CAPACITY] = dummyData;
+    hashTable->size--;
+    printf("\n Key (%d) has been removed \n", key);
+}
+
+int search(HashTable hashTable, int key) {
+    int index = hashCode(key);
+    if (hashTable.items[index].key == dummyData.key)
+    {
+        return -1;
+    }
+    int i = 0;
+    while(hashTable.items[(index+i)%CAPACITY].key != key && i < CAPACITY)
+    {
+        i++;
+    }
+    if(i == CAPACITY) return -1;
+    return index;
+}
+
+void destroyHashArray(HashTable *pHashTable) {
+    pHashTable->size = CAPACITY;//TODO
+    free(pHashTable->items);
+    pHashTable = NULL;
+}
+
+
